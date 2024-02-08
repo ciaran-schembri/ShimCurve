9d956eb8f2bec3c037d53e99c3361dbcbec83244
JamesRickards-Canada <james.rickards@colorado.edu>
2024-02-08 11:06:21 -0500

    Updated picture making.

--- pictures/picture-to-gp.m
+++ pictures/picture-to-gp.m
@@ -1,11 +1,11 @@
-//Attach("ShimCurve/pictures/picture-to-gp.m");
 
 intrinsic Norm(g::AlgQuatEnhElt) -> RngIntElt
   {Returns the norm of the AlgQuatEnhElt g}
   return Norm((g`element)[1]`element);
 end intrinsic;
 
-intrinsic PrepPictureData(O::AlgQuatOrd, Henhgens :: Tup) -> RngIntElt
+//Prints the data to algdat.dat, where you are given the order O and the subgroup H.
+intrinsic PrepPictureDataH(O::AlgQuatOrd, Henhgens :: Tup) -> RngIntElt
   {Saves the data to the data to a file, to be read by PARI/GP to compute the fundamental domain}
   SetOutputFile("ShimCurve/pictures/algdat.dat" : Overwrite := true);
   B := Algebra(O);//Retrieve the algebra
@@ -36,3 +36,15 @@ intrinsic PrepPictureData(O::AlgQuatOrd, Henhgens :: Tup) -> RngIntElt
   UnsetOutputFile();
   return 1;
 end intrinsic;
+
+//Does PrepPictureDataH for the full group H, i.e. the largest Atkin-Lehner quotient that we will consider.
+intrinsic PrepPictureDataMaximalH(O::AlgQuatOrd, mu::AlgQuatElt, N::RngIntElt) -> RngIntElt
+  {Saves the data to the data to a file, to be read by PARI/GP to compute the fundamental domain, for the maximal group H.}
+  AutmuO := Aut(O, mu);
+  G, Gelts := EnhancedImageGL4(AutmuO, O, N);
+  Genh := [ g`enhanced : g in Gelts];
+  GGL4gens := Generators(G);
+  Genhgens := < g`enhanced : g in Gelts | g`GL4 in GGL4gens >;
+  return PrepPictureDataH(O, Genhgens);
+end intrinsic;
+
