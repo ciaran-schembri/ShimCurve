fed74d7cfb9fa29d7f44af2c1fbab197c2e336b1
sarangop1728 <santiago.arango@emory.edu>
2024-02-09 15:12:38 -0500

    Fixed sintax issues with tables for X0(D;N).

--- code/tables/tablesX0DN.m
+++ code/tables/tablesX0DN.m
@@ -1,9 +1,9 @@
 intrinsic X0DNdata(DBound::RngIntElt, NBound::RngIntElt) -> Any
     {Outputs data table for the shimura curves X0(D;N), where D a quaternion discriminant and N coprime to D in the box D =< DBound and N <= NBound.}
 
-    filename := Sprintf("~/Desktop/ShimCurve/data/genera-tables/SignatureTableX0DN_%o_%o.txt", DBound, NBound);
-    Write(filename, Sprint("Glabel?all_degree1_points_known?autmuO_norms?bad_primes?cm_discriminants?coarse_class?coarse_class_num?coarse_index?coarse_label?coarse_num?conductor?curve_label?deg_mu?dims?discB?discO?fine_label?fine_num?fuchsian_index?galEnd?generators?genus?genus_minus_rank?gerbiness?has_obstruction?index?is_coarse?is_split?label?lattice_labels?lattice_x?level?level_is_squarefree?level_radical?log_conductor?models?mu_label?mults?name?newforms?nu2?nu3?nu4?nu6?num_bad_primes?num_known_degree1_noncm_points?num_known_degree1_points?obstructions?order_label?parents?parents_conj?pointless?power?psl2label?q_gonality?q_gonality_bounds?qbar_gonality?qbar_gonality_bounds?ram_data_elts?rank?reductions?scalar_label?simple?squarefree?torsion?trace_hash?traces"));
-    Write(filename, Sprint("text?boolean?integer[]?integer[]?integer[]?text?integer?integer?text?integer?integer[]?text?integer?integer[]?integer?integer?text?integer?integer?text?integer[]?integer?integer?integer?smallint?integer?boolean?boolean?text?text[]?integer[]?integer?boolean?integer?numeric?smallint?text?integer[]?text?text[]?integer?integer?integer?integer?integer?integer?integer?integer[]?text?text[]?integer[]?boolean?boolean?text?integer?integer[]?integer?integer[]?numeric[]?integer?text[]?text?boolean?boolean?integer[]?bigint?integer[]"));
+    filename := Sprintf("./data/genera-tables/SignatureTableX0DN_%o_%o.txt", DBound, NBound);
+    fprintf filename, "Glabel?all_degree1_points_known?autmuO_norms?bad_primes?cm_discriminants?coarse_class?coarse_class_num?coarse_index?coarse_label?coarse_num?conductor?curve_label?deg_mu?dims?discB?discO?fine_label?fine_num?fuchsian_index?galEnd?generators?genus?genus_minus_rank?gerbiness?has_obstruction?index?is_coarse?is_split?label?lattice_labels?lattice_x?level?level_is_squarefree?level_radical?log_conductor?models?mu_label?mults?name?newforms?nu2?nu3?nu4?nu6?num_bad_primes?num_known_degree1_noncm_points?num_known_degree1_points?obstructions?order_label?parents?parents_conj?pointless?power?psl2label?q_gonality?q_gonality_bounds?qbar_gonality?qbar_gonality_bounds?ram_data_elts?rank?reductions?scalar_label?simple?squarefree?torsion?trace_hash?traces \n";
+    fprintf filename, "text?boolean?integer[]?integer[]?integer[]?text?integer?integer?text?integer?integer[]?text?integer?integer[]?integer?integer?text?integer?integer?text?integer[]?integer?integer?integer?smallint?integer?boolean?boolean?text?text[]?integer[]?integer?boolean?integer?numeric?smallint?text?integer[]?text?text[]?integer?integer?integer?integer?integer?integer?integer?integer[]?text?text[]?integer[]?boolean?boolean?text?integer?integer[]?integer?integer[]?numeric[]?integer?text[]?text?boolean?boolean?integer[]?bigint?integer[] \n\n";
     // we only want D square-free, with an even number of prime factors.
     for D in [D : D in [6..DBound] | MoebiusMu(D) eq 1] do
         // we want N that are coprime to D and square-free.
@@ -46,7 +46,7 @@ intrinsic X0DNdata(DBound::RngIntElt, NBound::RngIntElt) -> Any
             log_conductor:="\\N";
             models:="\\N";
             mults:="\\N";
-            name:="\\N";
+            name:="X_0(" cat Sprintf("%o,%o)", D,N);
             newforms:="\\N";
             nu2:=EllipticInvariants(Gamma)[1][2];
             nu3:=EllipticInvariants(Gamma)[2][2];
@@ -68,10 +68,11 @@ intrinsic X0DNdata(DBound::RngIntElt, NBound::RngIntElt) -> Any
             pointless:="\\N";
             power:="\\N";
             psl2label:="\\N";
-            q_gonality:="\\N";
-            q_gonality_bounds:="\\N";
-            qbar_gonality:="\\N";
-            qbar_gonality_bounds:="\\N";
+            gonality_temp:=GonalityBoundListX0DN(D,N);
+            q_gonality:=gonality_temp[1];
+            q_gonality_bounds:=gonality_temp[2];
+            qbar_gonality:=gonality_temp[3];
+            qbar_gonality_bounds:=gonality_temp[4];
             ram_data_elts:="\\N";
             rank:="\\N";
             reductions:="\\N";
@@ -81,7 +82,7 @@ intrinsic X0DNdata(DBound::RngIntElt, NBound::RngIntElt) -> Any
             torsion:="\\N";
             trace_hash:="\\N";
             traces:="\\N";
-            Write(filename, Sprintf("%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o",Glabel,all_degree1_points_known,autmuO_norms,bad_primes,cm_discriminants,coarse_class,coarse_class_num,coarse_index,coarse_label,coarse_num,conductor,curve_label,deg_mu,dims,discB,discO,fine_label,fine_num,fuchsian_index,galEnd,generators,genus,genus_minus_rank,gerbiness,has_obstruction,index,is_coarse,is_split,label,lattice_labels,lattice_x,level,level_is_squarefree,level_radical,log_conductor,models,mu_label,mults,name,newforms,nu2,nu3,nu4,nu6,num_bad_primes,num_known_degree1_noncm_points,num_known_degree1_points,obstructions,order_label,parents,parents_conj,pointless,power,psl2label,q_gonality,q_gonality_bounds,qbar_gonality,qbar_gonality_bounds,ram_data_elts,rank,reductions,scalar_label,simple,squarefree,torsion,trace_hash,traces));
+            fprintf filename, Sprintf("%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o,%o \n",Glabel,all_degree1_points_known,autmuO_norms,bad_primes,cm_discriminants,coarse_class,coarse_class_num,coarse_index,coarse_label,coarse_num,conductor,curve_label,deg_mu,dims,discB,discO,fine_label,fine_num,fuchsian_index,galEnd,generators,genus,genus_minus_rank,gerbiness,has_obstruction,index,is_coarse,is_split,label,lattice_labels,lattice_x,level,level_is_squarefree,level_radical,log_conductor,models,mu_label,mults,name,newforms,nu2,nu3,nu4,nu6,num_bad_primes,num_known_degree1_noncm_points,num_known_degree1_points,obstructions,order_label,parents,parents_conj,pointless,power,psl2label,q_gonality,q_gonality_bounds,qbar_gonality,qbar_gonality_bounds,ram_data_elts,rank,reductions,scalar_label,simple,squarefree,torsion,trace_hash,traces);
         end for;
     end for;
     return Sprint("Table produced :)");
